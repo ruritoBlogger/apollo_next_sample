5ee20d5786b5a46eb42f5d8134ca0489a13a9fbc
ruritoBlogger <ruriro0125@gmail.com>
2023-02-04 15:21:28 +0900

    :wrench: ApolloProvider を設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,28 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  SuspenseCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/api/graphql",
+  cache: new InMemoryCache(),
+});
+
+const cache = new SuspenseCache();
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <ApolloProvider client={client} suspenseCache={cache}>
+        <body>{children}</body>
+      </ApolloProvider>
     </html>
-  )
+  );
 }
